9a4a930a0f08fb2643ae88006864b8e30ceb792b
VigorousBoy <994265947@qq.com>
2019-07-23 14:19:35 +0800

    branch分支

--- CodeIgniter-3.1.5/application/controllers/WeiXin.php
+++ CodeIgniter-3.1.5/application/controllers/WeiXin.php
@@ -71,7 +71,8 @@ class WeiXin extends CI_Controller
                 }else{
                     $third_uri.='?openid='.$_COOKIE[$this->openid];
                 }
-                header("Location:".$third_uri);
+                echo $third_uri;
+//                header("Location:".$third_uri);
             }
         }else{
             if($third_uri){
