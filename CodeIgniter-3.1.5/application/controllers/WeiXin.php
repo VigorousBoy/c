88d0d89bee5939a43159db4f39f818b411101862
VigorousBoy <994265947@qq.com>
2019-07-24 17:38:58 +0800

    branch分支

--- CodeIgniter-3.1.5/application/controllers/WeiXin.php
+++ CodeIgniter-3.1.5/application/controllers/WeiXin.php
@@ -228,6 +228,7 @@ class WeiXin extends CI_Controller
         $access_token=$access_token['access_token'];
         $target=$_GET['target'];
         $url = "https://api.weixin.qq.com/cgi-bin/media/uploadimg?access_token=".$access_token.'&type=image';
+        $target='/var/www/html/img/time.jpg';
         if (class_exists('CURLFile')) {
             $file = array("buffer"=>new CURLFile($target));  //$target即为logo图片路径
         } else {
