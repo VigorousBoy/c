386b823bf8eef0d1c96bcdeaece7bdad97e200aa
VigorousBoy <994265947@qq.com>
2019-07-30 17:00:55 +0800

    branch分支

--- wxapi/application/config/config.php
+++ wxapi/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://111.67.199.76/ci/c/wxapi';
 
 /*
 |--------------------------------------------------------------------------
